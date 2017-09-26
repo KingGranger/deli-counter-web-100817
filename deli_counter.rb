7c7f409232fd0728b8fc5c85a52d9ce56c2fcdf4
Brandon  Harris <cheeridon@gmail.com>
2017-09-26 20:39:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ end
 
 def take_a_number katz_deli, name
   katz_deli.push(name)
-  "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
+  "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 def now_serving katz_deli

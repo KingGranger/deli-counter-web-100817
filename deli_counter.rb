a795aa4a9459c57a9a76608e9b623aace9ef04f6
Brandon  Harris <cheeridon@gmail.com>
2017-09-26 21:03:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,11 @@ def take_a_number katz_deli, name
 end
 
 def now_serving katz_deli
-  katz_deli.length == 0 ? puts "There is nobody waiting to be served!" : puts "Currently serving #{katz_deli.shift}."
+  if katz_deli.size == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.shift}."
+  end
 end
 
 def line katz_deli

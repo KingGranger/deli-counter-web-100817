9149c681717bc0e1f473de6d67be139c2c89eb2d
Brandon  Harris <cheeridon@gmail.com>
2017-09-26 20:49:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,13 +6,13 @@ end
 
 def now_serving katz_deli
   puts "There is nobody waiting to be served!"if katz_deli.length == 0
-  puts "Currently serving #{katz_deli.shift}"
+  puts "Currently serving #{katz_deli.shift}."
 
 end
 
 def line katz_deli
   return "The line is currently empty." if katz_deli.length == 0
   on_line = ""
-  deli_line.each_with_index { |person, place| on_line << " #{place + 1}. #{person}"}
+  deli_line.each_with_index {|person, place| on_line << " #{place + 1}. #{person}"}
   "The line is currently:".concat(on_line)
 end

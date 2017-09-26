5cb64f9b9369dd838e36d58c180f31f1b48b3148
Brandon  Harris <cheeridon@gmail.com>
2017-09-26 20:41:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,4 @@
 
-def line katz_deli
-  return "The line is currently empty." if katz_deli.size == 0
-  on_line = ""
-  deli_line.each_with_index { |person, place| on_line << " #{place + 1}. #{person}"}
-  "The line is currently:".concat(on_line)
-end
-
 def take_a_number katz_deli, name
   katz_deli.push(name)
   "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
@@ -14,4 +7,11 @@ end
 def now_serving katz_deli
 
 
+end
+
+def line katz_deli
+  return "The line is currently empty." if katz_deli.size == 0
+  on_line = ""
+  deli_line.each_with_index { |person, place| on_line << " #{place + 1}. #{person}"}
+  "The line is currently:".concat(on_line)
 end
